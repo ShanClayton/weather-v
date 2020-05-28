2a2561fb22d1f0ca999972f8e292f6690300acfc
Shanaun <sclayton1183@gmail.com>
2020-05-27 23:40:36 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search__box">
+        <input type="text" class="search__bar" placeholder="Search..." />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "0e82bb16227c1a3b88661dd32729d7b1",
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Open Sans", sans-serif;
+}
+
+#app {
+  background-image: url(./assets/cold-bg.jpg);
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+main {
+  min-height: 100vh;
+  padding: 2.5rem;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search__box {
+  width: 100%;
+  margin-bottom: 3rem;
+}
+
+.search__box .search__bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 2rem;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search__box .search__bar:focus {
+  background-color: rgba(255, 255, 255, 0.75);
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  border-radius: 16px 0px 16px 0px;
+}
+</style>
